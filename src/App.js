31cfebe2f50a7cdc8c5214b85aa659edfd136cf7
Gulrez Bawa <gulrez0491@gmail.com>
2024-10-28 02:08:24 -0600

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import {useEffect,useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL;
+  const [data,setData]=useState(null)
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          {JSON.stringify(data)}
         </p>
         <a
           className="App-link"
